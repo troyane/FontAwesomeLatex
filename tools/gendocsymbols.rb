30f5eaab91654947d3cc631322a3ae3fa04935a7
Cicatrice <geo@localhost.localdomain>
2014-05-29 20:18:16 +0200

    Documentation and package update, based on <fontawesome>/src/icons.yml

---
+++ tools/gendocsymbols.rb
@@ -0,0 +1,38 @@
+require 'active_support/core_ext/string'
+require 'yaml'
+
+SOURCE_FILE=ARGV[0]
+CURRENT_VERSION='4.1'
+
+if SOURCE_FILE.nil? then
+	STDERR.puts "No source file given."
+	exit 2
+end
+
+icons = YAML.load_file(SOURCE_FILE)
+categories = {}
+
+icons['icons'].each do |icon|
+	cats = icon['categories']
+	cats.each do |cat|
+		categories[cat] = []  unless categories.keys.include? cat
+		categories[cat] << icon
+	end
+end
+
+categories.keys.each do |cat|
+	puts "\\subsection{#{cat}}"
+	puts "\\begin{showcase}"
+	categories[cat].each do |icon|
+		puts "\\showcaseicon{#{icon['id']}}{fa#{icon['id'].gsub('-','_').camelize}}{#{icon['created'].to_s == CURRENT_VERSION ? 'new' : ''}}% unicode:#{icon['unicode']} / created:#{icon['created']}"
+		unless icon['aliases'].nil? then
+			icon['aliases'].each do |aliasentry|
+				puts "\\showcaseicon{#{aliasentry}}{fa#{aliasentry.gsub('-','_').camelize}}{alias}"
+			end
+		end
+			
+	end
+	puts "\\end{showcase}"
+end
+
+
